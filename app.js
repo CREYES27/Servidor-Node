c1cb92a499b2de08ba0e61d7075b68a1acd9a99b
cesar Reyes <cjavierrv@gmail.com>
2022-06-22 15:02:47 -0300

    Listos para subir a produccion

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require('express')
+const multer  = require('multer')
+const sharp = require('sharp')
+const fs = require('fs')
+const { Console } = require('console')
+
+const storageStrategy =  multer.memoryStorage()
+//const upload = multer({ dest: 'uploads/' })  guarda el archivo en la carpeta uloads
+const upload = multer({ storage: storageStrategy }) //guarda el archivo en memoria (con la variable definida como: storageStrategy)
 const app = express()
 
+app.use(express.json())
 
 app.get('/', function (req, res) {
   res.send('Hola Mundo')
 })
 
-app.listen(3000)
+app.post('/imagen', upload.single('imagen'), async function(req, res){
+  const body = req.body
+  const imagen = req.file
+  const processedImage = sharp(imagen.buffer)
+  const resizedImage = processedImage.resize(800, 200, {
+    fit: "contain",
+    background: "#FFF"
+  })
+  const resizedImageBuffer = await resizedImage.toBuffer()
+
+  fs.writeFileSync('nuevaruta/prueba.png', resizedImageBuffer)
+  console.log(resizedImageBuffer)
+  res.send({ resizedImage: resizedImageBuffer})
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, function(){
+  console.log("Servidor escuchando en el puerto:", PORT)
+})
